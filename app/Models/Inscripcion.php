41e9c81c4112a22c537577cea4e53abcbfb47722
Ronald Diaz <ronalddiazp49@gmail.com>
2025-12-08 17:45:52 -0400

    se agrega el modelo para las inscripciones, se agregara mas para validar ciertos casos

---
+++ app/Models/Inscripcion.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inscripcion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'inscripcion';
+
+    protected $fillable = [
+        'codigo_inscripcion',
+        'estudiante_id',
+        'grupo_id',
+        'gestion_id',
+        'fecha_inscripcion',
+        'estado_id',
+    ];
+
+    protected $casts = [
+        'fecha_inscripcion' => 'date',
+    ];
+
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class, 'estudiante_id');
+    }
+
+    public function grupo()
+    {
+        return $this->belongsTo(Grupo::class, 'grupo_id');
+    }
+
+    public function gestion()
+    {
+        return $this->belongsTo(Gestion::class, 'gestion_id');
+    }
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class, 'estado_id');
+    }
+}
