43233c09e946f6f045c0d906eb94efd9b7dc7d62
Ronald Diaz <ronalddiazp49@gmail.com>
2025-12-08 17:47:06 -0400

    se agrega factory para inscripcion

---
+++ database/factories/InscripcionFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Estado;
+use App\Models\Estudiante;
+use App\Models\Gestion;
+use App\Models\Grupo;
+use App\Models\Inscripcion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Inscripcion>
+ */
+class InscripcionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Inscripcion::class;
+    public function definition(): array
+    {
+        return [
+            'codigo_inscripcion' => $this->faker->unique()->bothify('INS-####'),
+            'estudiante_id' => Estudiante::factory(),
+            'grupo_id' => Grupo::factory(),
+            'gestion_id' => Gestion::factory(),
+            'fecha_inscripcion' => $this->faker->date(),
+            'estado_id' => Estado::factory(),
+        ];
+    }
+}
