1df254e3aa0ee8188a99e04a21144cb7499f22e7
camILoDevs <camikko422@gmail.com>
2025-12-25 02:23:54 -0400

    add generator documents for page inscripciones

--- app/Filament/Inscripcion/Resources/Inscripcions/Tables/InscripcionsTable.php
+++ app/Filament/Inscripcion/Resources/Inscripcions/Tables/InscripcionsTable.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Filament\Inscripcion\Resources\Inscripcions\Tables;
-
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
+use Faker\Core\Color;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\Layout\Split;
 use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Actions\EditAction;
-use Filament\Actions\ViewAction;
 use Filament\Tables\Table;
 
 class InscripcionsTable
@@ -51,6 +52,13 @@ class InscripcionsTable
                             ->searchable()
                             ->sortable(),
 
+                        TextColumn::make('curso.nombre_completo')
+                            ->label('Curso')
+                            ->searchable()
+                            ->sortable()
+                            ->badge('secondary')
+                            ,
+
                         TextColumn::make('gestion.nombre')
                             ->label('Gestión')
                             ->searchable()
@@ -60,6 +68,7 @@ class InscripcionsTable
                             ->label('Fecha')
                             ->date()
                             ->sortable(),
+
 
                         TextColumn::make('estado.nombre')
                             ->label('Estado')
@@ -80,6 +89,45 @@ class InscripcionsTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                ActionGroup::make([
+                    Action::make('Hoja de Inscripción')
+                        ->icon(Heroicon::OutlinedDocumentArrowDown)
+                        ->color('secondary')
+                        ->modalHeading('Vista Previa - Hoja de Inscripción')
+                        ->modalSubmitActionLabel('Descargar PDF')
+                        ->modalCancelActionLabel('Cerrar')
+                        ->modalWidth('5xl')
+                        ->modalContent(fn ($record) => view('modals.preview-document', [
+                            'url' => route('documentos.inscripcion_hoja_datos.preview', $record->id),
+                            'downloadUrl' => route('documentos.inscripcion_hoja_datos.descargar', $record->id)
+                        ]))
+                        ->action(function ($record) {
+                            return redirect()->route('documentos.inscripcion_hoja_datos.descargar', $record->id);
+                        }),
+                    Action::make('Hoja de Servicios')
+                        ->icon(Heroicon::OutlinedDocumentArrowDown)
+                        ->color('secondary')
+                        ->modalHeading('Vista Previa - Hoja de Compromiso')
+                        ->modalSubmitActionLabel('Descargar PDF')
+                        ->modalCancelActionLabel('Cerrar')
+                        ->modalWidth('5xl')
+                        ->modalContent(fn ($record) => view('modals.preview-document', [
+                            'url' => route('documentos.preview.compromiso', $record->id),
+                            'downloadUrl' => route('documentos.descargar.compromiso', $record->id)
+                        ]))
+                        ->action(function ($record) {
+                            return redirect()->route('documentos.descargar.compromiso', $record->id);
+                        }),
+                    Action::make('Enviar Hojas por Email')
+                        ->icon('heroicon-o-envelope')
+                        ->color('secondary')
+                        ->openUrlInNewTab()
+                ])
+                    ->label('Documentos')
+                    ->outlined()
+                    ->icon(Heroicon::DocumentArrowDown)
+                    ->button()
+
             ])
 
             ->toolbarActions([
